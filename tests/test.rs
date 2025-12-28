e31f1c167a14e67c271016b5bf300a18598d2c78
AvhiMaz <avhimazumder5@outlook.com>
2025-12-28 13:47:07 +0530

    style: cargo fmt
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- tests/test.rs
+++ tests/test.rs
@@ -3,6 +3,9 @@ use amm_pinocchio::states::pool::Pool;
 #[test]
 fn test_pool_size() {
     println!("Size of Pool::LEN = {} bytes", Pool::LEN);
-    println!("Size of mem::size_of = {} bytes", core::mem::size_of::<Pool>());
+    println!(
+        "Size of mem::size_of = {} bytes",
+        core::mem::size_of::<Pool>()
+    );
     assert_eq!(Pool::LEN, core::mem::size_of::<Pool>());
 }
