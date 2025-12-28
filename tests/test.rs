5f836724c75357f47206a19ad8754f15c5e0be22
AvhiMaz <avhimazumder5@outlook.com>
2025-12-28 13:43:55 +0530

    chore: print mem::size_of
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- tests/test.rs
+++ tests/test.rs
@@ -2,6 +2,7 @@ use amm_pinocchio::states::pool::Pool;
 
 #[test]
 fn test_pool_size() {
-    println!("Pool::LEN = {} bytes", Pool::LEN);
+    println!("Size of Pool::LEN = {} bytes", Pool::LEN);
+    println!("Size of mem::size_of = {} bytes", core::mem::size_of::<Pool>());
     assert_eq!(Pool::LEN, core::mem::size_of::<Pool>());
 }
