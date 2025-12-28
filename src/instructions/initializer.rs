8140bbdc08d7208e1bfeb158a51e44306446c40c
AvhiMaz <avhimazumder5@outlook.com>
2025-12-28 19:56:28 +0530

    feat: complete initializer instruction
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/instructions/initializer.rs
+++ src/instructions/initializer.rs
@@ -1,12 +1,26 @@
+use bytemuck::{Pod, Zeroable};
 use pinocchio::{
-    ProgramResult, account_info::AccountInfo, program_error::ProgramError, pubkey::Pubkey,
+    ProgramResult,
+    account_info::AccountInfo,
+    instruction::{Seed, Signer},
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    sysvars::{Sysvar, rent::Rent},
+};
+use pinocchio_system::instructions::CreateAccount;
+
+use crate::{
+    constants::{LP_MINT_SEED, POOL_SEED, SYSTEM_PROGRAM_ID},
+    states::Pool,
+};
+use pinocchio_token::{
+    ID,
+    instructions::InitializeMint2,
+    state::{Mint, TokenAccount},
 };
 
-use crate::constants::SYSTEM_PROGRAM_ID;
-use pinocchio_token::{ID, state::TokenAccount};
-
 #[repr(C)]
-#[derive(Clone, Debug, Copy, PartialEq)]
+#[derive(Clone, Debug, Copy, PartialEq, Pod, Zeroable)]
 pub struct InitializeInstructionData {
     pub fee_rate: u16,
 }
@@ -16,9 +30,9 @@ impl InitializeInstructionData {
 }
 
 pub fn process_initialize(
-    _program_id: &Pubkey,
+    program_id: &Pubkey,
     accounts: &[AccountInfo],
-    _instruction: &[u8],
+    instruction: &[u8],
 ) -> ProgramResult {
     let [
         authority,
@@ -77,6 +91,102 @@ pub fn process_initialize(
     if vault_b_account.amount() != 0 {
         return Err(ProgramError::AccountAlreadyInitialized);
     }
+
+    let ix_data = bytemuck::try_from_bytes::<InitializeInstructionData>(instruction)
+        .map_err(|_| ProgramError::InvalidInstructionData)?;
+
+    let fee_rate = ix_data.fee_rate;
+
+    //  - 1 basis point = 0.01%
+    //  - 10000 basis points = 100%
+    if fee_rate > 10000 {
+        return Err(ProgramError::InvalidArgument);
+    }
+
+    let (pool_pda, pool_bump) = pinocchio::pubkey::find_program_address(
+        &[
+            POOL_SEED.as_bytes(),
+            token_a.key().as_ref(),
+            token_b.key().as_ref(),
+        ],
+        program_id,
+    );
+
+    if pool.key() != &pool_pda {
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let (lp_mint_pda, lp_mint_pda_bump) = pinocchio::pubkey::find_program_address(
+        &[LP_MINT_SEED.as_bytes(), pool.key().as_ref()],
+        program_id,
+    );
+
+    if lp_mint.key() != &lp_mint_pda {
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let rent = Rent::get()?;
+
+    let binding = [pool_bump];
+    let pool_seed = [
+        Seed::from(POOL_SEED.as_bytes()),
+        Seed::from(token_a.key().as_ref()),
+        Seed::from(token_b.key().as_ref()),
+        Seed::from(&binding),
+    ];
+
+    let pool_seed_signer = Signer::from(&pool_seed[..]);
+
+    (CreateAccount {
+        from: authority,
+        to: pool,
+        space: Pool::LEN as u64,
+        lamports: rent.minimum_balance(Pool::LEN),
+        owner: program_id,
+    })
+    .invoke_signed(&[pool_seed_signer])?;
+
+    let mut pool_data = pool.try_borrow_mut_data()?;
+    let pool_state = Pool::load_mut(&mut pool_data)?;
+
+    pool_state.set_inner_full(Pool {
+        authority: *pool.key(),
+        token_a: *token_a.key(),
+        token_b: *token_b.key(),
+        lp_mint: *lp_mint.key(),
+        vault_a: *vault_a.key(),
+        vault_b: *vault_b.key(),
+        reserve_a: 0,
+        reserve_b: 0,
+        fee_rate,
+        bump: pool_bump,
+        lp_mint_bump: lp_mint_pda_bump,
+        _padding: [0; 4],
+    });
+
+    let binding = [lp_mint_pda_bump];
+    let lp_mint_seed = [
+        Seed::from(LP_MINT_SEED.as_bytes()),
+        Seed::from(pool.key().as_ref()),
+        Seed::from(&binding),
+    ];
+
+    (CreateAccount {
+        from: authority,
+        to: lp_mint,
+        space: Mint::LEN as u64,
+        lamports: rent.minimum_balance(Mint::LEN),
+        owner: &ID,
+    })
+    .invoke_signed(&[Signer::from(&lp_mint_seed[..])])?;
+
+    InitializeMint2 {
+        mint: lp_mint,
+        decimals: 6,
+        mint_authority: pool.key(),
+        freeze_authority: None,
+    }
+    .invoke_signed(&[Signer::from(&lp_mint_seed[..])])?;
 
     Ok(())
 }
