fa6aab867b09f092fb8b849da12eadbb5c20973c
AvhiMaz <avhimazumder5@outlook.com>
2026-01-01 19:24:36 +0530

    chore: define acc
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/instructions/add_liquidity.rs
+++ src/instructions/add_liquidity.rs
@@ -1,9 +1,40 @@
-use pinocchio::{ProgramResult, account_info::AccountInfo, pubkey::Pubkey};
+use bytemuck::{Pod, Zeroable};
+use pinocchio::{
+    ProgramResult, account_info::AccountInfo, program_error::ProgramError, pubkey::Pubkey,
+};
+
+#[repr(C)]
+#[derive(Clone, Debug, Copy, PartialEq, Pod, Zeroable)]
+pub struct AddLiquidityInstructionData {
+    pub amount_a: u64,      // amount of token a
+    pub amount_b: u64,      // amount of token b
+    pub min_lp_amount: u64, // slippage
+}
+
+impl AddLiquidityInstructionData {
+    pub const LEN: usize = core::mem::size_of::<Self>();
+}
 
 pub fn process_add_liquidity(
     _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
+    accounts: &[AccountInfo],
     _instruction: &[u8],
 ) -> ProgramResult {
+    let [
+        user,
+        pool,
+        lp_mint,
+        vault_a,
+        vault_b,
+        user_token_a,
+        user_token_b,
+        user_lp_token,
+        token_program,
+        _remaining @ ..,
+    ] = accounts
+    else {
+        return Err(ProgramError::InvalidAccountData);
+    };
+
     Ok(())
 }
