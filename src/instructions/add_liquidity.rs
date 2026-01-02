cd6b66b1566aa5e162ba634ffe551834ba4cc38f
AvhiMaz <avhimazumder5@outlook.com>
2026-01-02 14:16:08 +0530

    chore: add validation and se/de
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/instructions/add_liquidity.rs
+++ src/instructions/add_liquidity.rs
@@ -2,6 +2,12 @@ use bytemuck::{Pod, Zeroable};
 use pinocchio::{
     ProgramResult, account_info::AccountInfo, program_error::ProgramError, pubkey::Pubkey,
 };
+use pinocchio_token::{
+    ID,
+    state::{Mint, TokenAccount},
+};
+
+use crate::states::Pool;
 
 #[repr(C)]
 #[derive(Clone, Debug, Copy, PartialEq, Pod, Zeroable)]
@@ -18,23 +24,79 @@ impl AddLiquidityInstructionData {
 pub fn process_add_liquidity(
     _program_id: &Pubkey,
     accounts: &[AccountInfo],
-    _instruction: &[u8],
+    instruction: &[u8],
 ) -> ProgramResult {
     let [
-        _user,
-        _pool,
-        _lp_mint,
-        _vault_a,
-        _vault_b,
-        _user_token_a,
-        _user_token_b,
-        _user_lp_token,
-        _token_program,
+        user,
+        pool,
+        lp_mint,
+        vault_a,
+        vault_b,
+        user_token_a,
+        user_token_b,
+        user_lp_token,
+        token_program,
         _remaining @ ..,
     ] = accounts
     else {
         return Err(ProgramError::InvalidAccountData);
     };
+
+    if !user.is_signer() {
+        return Err(ProgramError::IncorrectAuthority);
+    }
+
+    if instruction.len() != AddLiquidityInstructionData::LEN {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    let amount_a = u64::from_le_bytes(
+        instruction[0..8]
+            .try_into()
+            .map_err(|_| ProgramError::InvalidInstructionData)?,
+    );
+
+    let amount_b = u64::from_le_bytes(
+        instruction[8..16]
+            .try_into()
+            .map_err(|_| ProgramError::InvalidInstructionData)?,
+    );
+
+    let min_lp_amount = u64::from_le_bytes(
+        instruction[16..24]
+            .try_into()
+            .map_err(|_| ProgramError::InvalidInstructionData)?,
+    );
+
+    if amount_a == 0 || amount_b == 0 {
+        return Err(ProgramError::InvalidAccountData);
+    };
+
+    if token_program.key() != &ID {
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let mut pool_data = pool.try_borrow_mut_data()?;
+    let pool_state = Pool::load_mut(&mut pool_data)?;
+
+    let lp_mint_acc = Mint::from_account_info(lp_mint)?;
+    let vault_a_acc = TokenAccount::from_account_info(vault_a)?;
+    let vault_b_acc = TokenAccount::from_account_info(vault_b)?;
+    let user_token_a_acc = TokenAccount::from_account_info(user_token_a)?;
+    let user_token_b_acc = TokenAccount::from_account_info(user_token_b)?;
+    let user_lp_token_acc = TokenAccount::from_account_info(user_lp_token)?;
+
+    if user_token_a_acc.mint() != &pool_state.token_a {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    if user_token_b_acc.mint() != &pool_state.token_b {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    if user_lp_token_acc.mint() != &pool_state.lp_mint {
+        return Err(ProgramError::InvalidInstructionData);
+    }
 
     Ok(())
 }
